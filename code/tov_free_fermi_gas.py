a45c1e7cfc66d85acaf7d8bd2f67b125671efb0c
Herman Sletmoen <hermansletmoen@gmail.com>
2021-11-05 16:24:13 +0100

    Clean up code

--- code/tov_free_fermi_gas.py
+++ code/tov_free_fermi_gas.py
@@ -1,50 +1,29 @@
 #!/usr/bin/python3
 
-from constants import *
 from tov_solve import massradiusplot, r0, m0
 import utils
 import numpy as np
+import scipy.optimize
 
-def ϵUR(P): return 3*P
-
+from constants import *
 m = 1.67492749804e-27 # kg
-ratio = m**8*c**6*r0**6 / (m0**2*ħ**6)
 
-def ϵNR(P): return (5**3*4**2 / (3**4*π**2) * ratio * np.abs(P)**3)**(1/5)
+# Non-relativistic limit
+def ϵNR(P):
+    factorNR = 5**3*4**2 / (3**4*π**2) * m**8*c**6*r0**6 / (m0**2*ħ**6)
+    return (factorNR * np.abs(P)**3)**(1/5)
 
-# Px(1e7) ≈ 1e28, so need to consider x ∈ [0, 1e7] for P ∈ [0, 1e28]
-def Px(x): return m**4*c**3*r0**3 / (18*π*m0*ħ**3) * ((2*x**3 - 3*x) * np.sqrt(x**2 + 1) + 3*np.arcsinh(x))
+massradiusplot(ϵNR, (1e-6, 1e21), tolD=0.05, tolP=1e-5, maxdr=2e-3, outfile="data/nr.dat")
+
+# Arbitrary Fermi momentum
 def ϵGR(P):
-    P = np.abs(P) # TODO: implications?
-    def f(x): return Px(x) - P
-    sol = scipy.optimize.root_scalar(f, method="bisect", bracket=(0, 1e18))
+    P = np.abs(P) # avoid (small) negative pressure TODO: implications of this?
+    factorGR = m**4*c**3*r0**3 / (18*π*m0*ħ**3)
+    f = lambda x, P: factorGR * ((2*x**3 - 3*x) * np.sqrt(x**2 + 1) + 3*np.arcsinh(x)) - P
+    sol = scipy.optimize.root_scalar(f, method="bisect", bracket=(0, 1e18), args=(P))
     assert sol.converged, "ERROR: equation of state root finder did not converge"
     x = sol.root
-    ϵ = m**4*c**3*r0**3 / (6*π*m0*ħ**3) * ((2*x**3+x) * np.sqrt(x**2 + 1) - np.arcsinh(x))
+    ϵ = 3*factorGR * ((2*x**3+x) * np.sqrt(x**2 + 1) - np.arcsinh(x))
     return ϵ
 
-# rs, ms, Ps = soltov(ϵNR, 2.4)
-
-#Ps, Ms, Rs = massradiusplot(ϵNR, 1e-6, 1e10, tolD=0.05, tolP=1e-5, max_step=5e-4, visual=True) # for full curve
-
-#Ps, Ms, Rs = massradiusplot(ϵNR, 1e1, 1e21, tolD=0.04, tolP=1e-3, max_step=2e-4, visual=True) # for spiral only
-
-#Ps, Ms, Rs = massradiusplot(ϵNR, 1e-6, 1e21, tolD=0.05, tolP=1e-5, max_step=2e-4, visual=True) # everything?
-#writecols([Ps, Ms, Rs], ["P", "M", "R"], "data/nr.dat")
-
-# TODO: P never 0, cannot integrate to np.inf
-#Ps, Ms, Rs = massradiusplot(ϵUR, 1e-0, 1e4, tolD=0.1, tolP=1e-1, max_step=1e-2, visual=True) # everything?
-#writecols([Ps, Ms, Rs], ["P", "M", "R"], "data/ur.dat")
-
-# Everything for GR
-#Ps, Ms, Rs = massradiusplot(ϵGR, 1e-6, 1e17, tolD=0.05, tolP=1e-5, max_step=2e-3, visual=True)
-#writecols([Ps, Ms, Rs], ["P", "M", "R"], "data/gr.dat")
-
-# TESTING
-Ps, Ms, Rs = massradiusplot(ϵNR, 1e-6, 1e3, tolD=0.5, tolP=1e-5, visual=False) # for full curve
-utils.writecols([Ps, Ms, Rs], ["P", "M", "R"], "data/test.dat")
-
-
-#plt.plot(Rs, Ms, "-ko")
-#plt.show()
-#Ps, Ms, Rs = massradiusplot(ϵNR, 1e-2, 1e2, tolD=0.5, tolP=1e-2, max_step=1e-3, visual=False) # for spiral
+massradiusplot(ϵGR, (1e-6, 1e17), tolD=0.1, tolP=1e-5, maxdr=2e-3, outfile="data/gr.dat")
--- code/tov_solve.py
+++ code/tov_solve.py
@@ -2,52 +2,48 @@
 
 import numpy as np
 import scipy.integrate
-import scipy.optimize
 import matplotlib.pyplot as plt
 
-from constants import *
+from constants import c
 m0 = 1.98847e30 # kg
 r0 = 10e3 # m
 G = 6.67430e-11 / (r0*c**2/m0) # dimensionless
 
-"""
-ϵ0 = m0*c**2 / (4/3*π*r0**3)
-print(f"ϵ0 = {ϵ0}")
+def soltov(ϵ, P0, maxdr=1e-3):
+    print(f"Solving TOV with ϵ = {ϵ.__name__}, P0 = {P0}, maxdr = {maxdr} ...")
 
-kB = 1.380649e-23
-T = 1e6 # https://en.wikipedia.org/wiki/Neutron_star#Mass_and_temperature
-print(f"m c^2 / k_B T = {m * c**2 / (kB * T)} >> 1")
-"""
-
-def soltov(ϵ, P0, max_step=1e-3):
-    def f(r, y):
+    def rhs(r, y):
         m, P = y[0], y[1]
         dmdr = 3*r**2*ϵ(P)
-        dPdr = -G*m*ϵ(P)/r**2 * (1 + P/ϵ(P)) * (1 + 3*r**2*P/ϵ(P)) / (1 - 2*G*m/r) if r != 0 else 0 # avoid division by r=0 (correct because m=0 when r=0)
+        if r == 0:
+            dPdr = 0 # avoid division by r = 0 (m = 0 implies dPdr = 0)
+        else:
+            dPdr = -G*m*ϵ(P)/r**2 * (1 + P/ϵ(P)) * (1 + 3*r**2*P/ϵ(P)) / (1 - 2*G*m/r)
         return [dmdr, dPdr]
 
-    # stop integration when P == 0 and use this as last point
-    def e(r, y):
+    def terminator(r, y):
         m, P = y[0], y[1]
         return P - 0
-    e.terminal = True
-    e.direction = -1
+    terminator.terminal = True # stop integration when P == 0, use as last point
 
-    res = scipy.integrate.solve_ivp(f, (0, np.inf), (0, P0), events=e, max_step=max_step)
+    res = scipy.integrate.solve_ivp(
+        rhs, (0, np.inf), (0, P0), events=terminator, max_step=maxdr
+    )
     assert res.success, "ERROR: " + res.message
-    rs, ms, Ps = res.t, res.y[0,:], res.y[1,:]
 
+    rs, ms, Ps = res.t, res.y[0,:], res.y[1,:]
     return rs, ms, Ps
     
-def massradius(ϵ, P0, max_step=1e-3):    
-    rs, ms, Ps = soltov(ϵ, P0, max_step=max_step)
+def massradius(ϵ, P0, maxdr=1e-3):    
+    rs, ms, Ps = soltov(ϵ, P0, maxdr=maxdr)
     R, M = rs[-1], ms[-1]
     return R, M
 
 # Bisect [P1, P2] to make points evenly
-def massradiusplot(ϵ, P1, P2, tolD=1e-5, tolP=1e-6, max_step=1e-3, visual=False):
-    R1, M1 = massradius(ϵ, P1, max_step=max_step)
-    R2, M2 = massradius(ϵ, P2, max_step=max_step)
+def massradiusplot(ϵ, P1P2, tolD=1e-5, tolP=1e-6, maxdr=1e-3, outfile="", visual=False):
+    P1, P2 = Pinterval[0], Pinterval[1]
+    R1, M1 = massradius(ϵ, P1, maxdr=maxdr)
+    R2, M2 = massradius(ϵ, P2, maxdr=maxdr)
     Ps, Ms, Rs = [P1, P2], [M1, M2], [R1, R2]
 
     if visual:
@@ -58,20 +54,21 @@ def massradiusplot(ϵ, P1, P2, tolD=1e-5, tolP=1e-6, max_step=1e-3, visual=False
     while i < len(Ps) - 1:
         P1, M1, R1 = Ps[i],   Ms[i],   Rs[i]
         P2, M2, R2 = Ps[i+1], Ms[i+1], Rs[i+1]
+
+        # Split intervals based on Euclidean distance between (R, M)-points in plot
+        # But make sure P1, P2 do not get too close, otherwise algorithm gets stuck
         D = np.sqrt((R1 - R2)**2 + (M1 - M2)**2)
-
-        progress = int((P1 - Ps[0]) / (Ps[-1] - Ps[0]) * 100) # fraction of pressures covered
-        print(f"\r{progress:0d} %", end="") # print progress
-
         if D > tolD and P2 - P1 > tolP:
             # split [P1, P2] into [P1, (P1+P2)/2] and [(P1+P2)/2, P2]
             P3 = (P1 + P2) / 2
-            R3, M3 = massradius(ϵ, P3, max_step=max_step)
+            R3, M3 = massradius(ϵ, P3, maxdr=maxdr)
             Ps.insert(i+1, P3)
             Ms.insert(i+1, M3)
             Rs.insert(i+1, R3)
+
             if visual:
-                # animate, inspired by https://stackoverflow.com/a/10944967
+                # Animate plot in real-time for immediate feedback
+                # inspired by https://stackoverflow.com/a/10944967
                 graph.set_data(Rs, Ms)
                 plt.gca().relim() # autoscale only works in animation after this
                 plt.autoscale()
@@ -80,9 +77,12 @@ def massradiusplot(ϵ, P1, P2, tolD=1e-5, tolP=1e-6, max_step=1e-3, visual=False
         else:
             i += 1
 
-    print("\r100 %") # end progress line
     if visual:
         plt.ioff() # leave original state
         plt.show() # leave final plot open
 
+    if outfile != "":
+        utils.writecols([Ps, Ms, Rs], ["P", "M", "R"], outfile)
+        print(f"Wrote (P, M, R) to {outfile}")
+    
     return Ps, Ms, Rs
