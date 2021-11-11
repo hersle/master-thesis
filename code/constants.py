a5ec04662889ff0cc19eaf72921a98af18eeafcb
Herman Sletmoen <hermansletmoen@gmail.com>
2021-11-11 15:25:22 +0100

    Added maximum points to mass-radius plot

--- code/constants.py
+++ code/constants.py
@@ -11,3 +11,8 @@ m0 = 1.98847e30 # kg, solar mass
 G = scipy.constants.G / (r0*c**2/m0) # dimensionless gravitational constant
 b = 3
 ϵ0 = m0*c**2 / (4*π*r0**3/b)
+
+print(f"r0 = {r0}")
+print(f"m0 = {m0}")
+print(f"G  = {G}")
+print(f"ϵ0 = {ϵ0}")
