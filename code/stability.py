bc15522f646ea067b6f795b062e90e8a94416d3c
Herman Sletmoen <hermansletmoen@gmail.com>
2021-11-13 12:45:11 +0100

    Have working shooting method search example

--- code/stability.py
+++ code/stability.py
@@ -33,7 +33,7 @@ def shoot(r, Π, Q, W, ω2, j=50):
         """
         denom = 2*Π[i]/hp+dΠdr[i]
         factor = 1/denom
-        print(factor)
+        #print(factor)
         if np.abs(factor) > 1e2:
             # "handle" blowup without oscillating back
             u[i+1] = u[i] + (u[i]-u[i-1]) * (r[i+1]-r[i]) / (r[i]-r[i-1]) # linterp last few points
@@ -53,9 +53,7 @@ def count_nodes(u):
             nodes += 1
     return nodes
 
-def search(r, Π, Q, W):
-    N = 0 # target
-
+def search(r, Π, Q, W, N=0):
     def increaseuntil(ω20, cond, sign=+1):
         ω2 = ω20
         u, n = shoot(r, Π, Q, W, ω2)
@@ -69,58 +67,69 @@ def search(r, Π, Q, W):
     def decreaseuntil(ω20, cond):
         return increaseuntil(ω20, cond, sign=-1)
 
-    def bisectuntil(ω21, ω22, tol=1e-5):
+    def bisectuntil(ω21, ω22, tol=1e-5, plot=False):
         u1, n1 = shoot(r, Π, Q, W, ω21)
         u2, n2 = shoot(r, Π, Q, W, ω22)
         while ω22 - ω21 > tol:
             ω23 = (ω21 + ω22) / 2
             u3, n3 = shoot(r, Π, Q, W, ω23)
             print(f"ω2 = {ω23} -> {n3} nodes")
-            plt.plot(r, u3)
+            if plot:
+                plt.plot(r, u3, label=f"n = {n3}, ω2 = {ω23}")
             if n3 > N:
                 ω22, u2, n2 = ω23, u3, n3
             else:
                 ω21, u1, n1 = ω23, u3, n3
         ω2 = (ω21 + ω22) / 2
         u, n = shoot(r, Π, Q, W, ω23)
-        plt.show()
+        if plot:
+            plt.ylim(-1e3, +1e3)
+            plt.legend()
+            plt.show()
         return ω2, u, n
+
+    ω20 = 0
+    u, n = shoot(r, Π, Q, W, ω20)
+    if n > N:
+        # ω20 is an upper bound
+        ω22 = ω20
+        ω21 = decreaseuntil(ω20, lambda u, n: n <= N)
+    else:
+        # ω20 is a lower bound
+        ω21 = ω20
+        ω22 = increaseuntil(ω20, lambda u, n: n > N)
+
+    """ 
+    #DEBUG
+    print(ω21, ω22)
+    u1, n1 = shoot(r, Π, Q, W, ω21)
+    u2, n2 = shoot(r, Π, Q, W, ω22)
+    plt.plot(r, u1, label=f"{n1}, {ω21}")
+    plt.plot(r, u2, label=f"{n2}, {ω22}")
+    plt.legend()
+    plt.ylim(-1e2, +1e2)
+    plt.show()
+    """
+
+    ω2, u, n = bisectuntil(ω21, ω22, plot=True)
+    return ω2, u, n
+
 
 def fundamental_mode(r, m, P, α, ϵ):
     dPdr = np.gradient(P, r)
     dPdϵ = np.gradient(P, ϵ)
 
-    # r, m, P, α, ϵ, dPdr, dϵdr = r[2:], m[2:], P[2:], α[2:], ϵ[2:], dPdr[2:], dϵdr[2:] # avoid division by zero at r=0
-
-    M = m[-1]
-    R = r[-1]
-
-    print(1/2*np.log(1-2*G*M/R), "should =", α[-1])
-
     β = -1/2*np.log(1-2*G*m/r) # β(0) = 0, avoid division by 0, already dimensionless
     Γ = (P + ϵ) / P * dPdϵ # already dimensionless
     Π = np.exp(β+3*α)/r**2 * Γ * P
     Q = -4*np.exp(β+3*α)/r**3*dPdr - 8*π*(G/4*π)*np.exp(3*β+3*α)/r**2*P*(ϵ+P) + np.exp(β+3*α)*dPdr**2 / (r**2*(ϵ+P))
-    Q[-10:] = 0 # TODO: reliably set last Qs to 0
+    #Q[-10:] = 0 # TODO: reliably set last Qs to 0 ?
     W = np.exp(3*β+α)*(ϵ+P)/r**2
 
-    # doesnt matter if i divide each term by some maximum number
+    # TODO: can i divide Π, Q, W by their maximums to make numbers more handleable?
+
     """
-    Πmax = np.max(np.abs(Π[2:]))
-    Qmax = np.max(np.abs(Q[2:]))
-    Wmax = np.max(np.abs(W[2:]))
-    max = np.max([Πmax, Qmax, Wmax])
-    Π /= max
-    Q /= max
-    W /= max
-    """
-
-    #plt.plot(r[50:], Q[50:])
-    #plt.show()
-
-    #search(r, Π, Q, W)
-    #return
-
+    # TRY OUT
     ω2 = np.linspace(-5, +5, 10)
 
     for ω2 in ω2:
@@ -129,3 +138,10 @@ def fundamental_mode(r, m, P, α, ϵ):
     plt.ylim(-1e3, +1e3)
     plt.legend(loc="upper left")
     plt.show()
+    """
+
+    #plt.plot(r[50:], Q[50:])
+    #plt.show()
+
+    ω2, u, n = search(r, Π, Q, W)
+    return ω2, u
