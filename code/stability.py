Fixed units in Sturm-Liouville problem and got correct result!

code/stability.py
@@ -113,7 +113,7 @@ def coeffs(r, m, P, α, ϵ):
     Γ = (P + ϵ) / P * dPdϵ # already dimensionless
 
     Π = np.exp(β+3*α)/r**2 * Γ * P
-    Q = -4*np.exp(β+3*α)/r**3*dPdr - 8*π*(G/4*π)*np.exp(3*β+3*α)/r**2*P*(ϵ+P) + np.exp(β+3*α)*dPdr**2 / (r**2*(ϵ+P))
+    Q = -4*np.exp(β+3*α)/r**3*dPdr - (8*π*G/(4*π/3))*np.exp(3*β+3*α)/r**2*P*(ϵ+P) + np.exp(β+3*α)*dPdr**2 / (r**2*(ϵ+P))
     W = np.exp(3*β+α)*(ϵ+P)/r**2
 
     return Π, Q, W

code/tov_free_fermi_gas.py
@@ -37,14 +37,9 @@ def ϵGR(P):
 massradiusplot(ϵGR, (1e-6, 1e7), tolD=0.05, tolP=1e-5, maxdr=1e-3, stability=True, visual=True, outfile="data/gr2.dat")
 
 """
-for P0 in np.geomspace(1e-6, 1e7, 10):
-    r, m, P, α, ϵ = soltov(ϵNR, P0)
-    ω2, u = eigenmode(r, m, P, α, ϵ, 0, progress=True)
-"""
-
 r, m, P, α, ϵ = soltov(ϵGR, 1e3)
 ω2s, us = eigenmode(r, m, P, α, ϵ, [0], plot=True, outfileshoot="data/shoot.dat")# , outfile="data/nmodes.dat")
-exit()
+"""
 
 #P0 = 1e-1
 P0 = 5e2