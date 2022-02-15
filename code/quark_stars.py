308bdedade807a9d7098969b25443259ae43af0b
Herman Sletmoen <hermansletmoen@gmail.com>
2022-02-15 14:56:37 +0100

    Try to solve TOV with 2 EOSs

--- code/quark_stars.py
+++ code/quark_stars.py
@@ -18,9 +18,6 @@ mq0 = 300
 fπ = 93
 me = 0
 
-neutral = True
-μside = "right" # left or right
-
 g = mq0 / fπ
 h = fπ * mπ**2
 m = np.sqrt(1/2*(mσ**2-3*h/fπ))
@@ -35,15 +32,20 @@ def pf(μ, m): return np.real(np.sqrt(μ**2 - m**2 + 0j))
 def xf(μ, m): return pf(μ, m) / np.abs(m)
 def n(μ, m): return pf(μ, m)**3 / (3*π**2)
 
-def μelim(σ, μu, neutral, side, μdmax=1e5, verbose=False, ftol=0):
+def μelim(σ, μu, neutral, side, neglecte, μdmax=1e5, verbose=False, ftol=0):
     assert np.shape(σ) == np.shape(μu), "σ and μu has different shapes"
     if np.ndim(σ) >= 1:
-        return np.array([μelim(σ[i], μu[i], μdmax=μdmax, verbose=verbose, ftol=ftol, neutral=neutral, side=side) for i in range(0, len(σ))])
+        return np.array([μelim(σ[i], μu[i], neutral, side, neglecte, μdmax=μdmax, verbose=verbose, ftol=ftol) for i in range(0, len(σ))])
         
     if not neutral:
         μd = μu
         μe = 0
     else:
+        if neglecte:
+            μd = 2**(1/3) * μu # from Two Lectures on Color Superconductivity (Shovkovy), page 1337
+            μe = μu/4
+            return np.array([μu, μd, μe])
+        
         if μu <= mq(σ): # then nu = 0, and ne = nd = 0 is a trivial solution
             μe = 0 # so ne = 0
             μd = μu # from chemical equilibrium (μe = μd - μu)
@@ -69,10 +71,10 @@ def μelim(σ, μu, neutral, side, μdmax=1e5, verbose=False, ftol=0):
 
             μe = μd - μu
 
-            if f(μd0) > 0 and μd > 300:
+            if False and μu > 300:
                 print(f"σ = {σ}, μu = {μu}")
-                print(f"min:  f(μd = {μd0}) = {f(μd0)}")
-                print(f"zero: f(μd = {μd}) = {f(μd)}")
+                #print(f"min:  f(μd = {μd0}) = {f(μd0)}")
+                #print(f"zero: f(μd = {μd}) = {f(μd)}")
                 print(f"u   : nu = {n(μu,mq(σ))}")
                 print(f"d   : nd = {n(μd,mq(σ))}")
                 print(f"e   : ne = {n(μe,me)}")
@@ -104,33 +106,35 @@ def ωf(σ, μ, verbose=False):
     #ωd = -Nc/(24*π**2) * ((2*μd**2-5*mq(σ)**2)*μd*np.real(np.sqrt(μd**2-mq(σ)**2+0j)) + 3*mq(σ)**4*np.arcsinh(np.real(np.sqrt(μd**2/mq(σ)**2-1+0j))))
     return ω0 + ωe + ωu + ωd
 
-def σμω(σ, μu, neutral, μside, verbose=False):
-    μ = μelim(σ, μu, neutral, μside) # [nσ, nμu, 3]
+def σμω(σ, μu, neutral, μside, neglecte, verbose=False):
+    μ = μelim(σ, μu, neutral, μside, neglecte) # [nσ, nμu, 3]
     ω = ωf(σ, μ)
     μu, μd, μe = μ[...,0], μ[...,1], μ[...,2]
     return σ, μu, μd, μe, ω
 
-def σμω0(μu, neutral, μside, σmax=200):
+def σμω0(μu, neutral, μside, neglecte, σmax=200):
     if np.ndim(μu) >= 1:
-        return np.array([σμω0(μu, neutral, μside, σmax=σmax) for μu in μu])
+        return np.array([σμω0(μu, neutral, μside, neglecte, σmax=σmax) for μu in μu])
 
     def ωf2(σ):
         μus = np.full(np.shape(σ), μu)
-        _, _, _, _, ω = σμω(σ, μus, neutral, μside)
+        _, _, _, _, ω = σμω(σ, μus, neutral, μside, neglecte)
         return ω
     σ0 = scipy.optimize.minimize_scalar(ωf2, bounds=(0, σmax), method="bounded").x
-    μ0 = μelim(σ0, μu, neutral, μside)
+    μ0 = μelim(σ0, μu, neutral, μside, neglecte)
     μu0, μd0, μe0 = μ0[...,0], μ0[...,1], μ0[...,2]
     ω0 = ωf(σ0, μ0)
     return σ0, μu0, μd0, μe0, ω0
 
-def eos(neutral, μside, name="ϵ"):
-    μu = np.linspace(0, 1e4, 500)
-    ret = σμω0(μu, neutral, μside)
+def eos(neutral, μside, neglecte, B=0, name="ϵ", plotfirst=False):
+    μu = np.concatenate([np.linspace(0, 250, 50), np.linspace(250, 400, 200), np.linspace(400, 10000, 50)])
+    #μu = np.linspace(0, 1000, 500)
+    ret = σμω0(μu, neutral, μside, neglecte)
     σ0, μu0, μd0, μe0, ω0 = ret[:,0], ret[:,1], ret[:,2], ret[:,3], ret[:,4]
     nu0, nd0, ne0 = n(μu0, mq(σ0)), n(μd0, mq(σ0)), n(μe0, me)
     P0 = -ω0
     P0 = P0 - P0[0]
+    P0 -= B # bag constant TODO: which value?
     ϵ0 = μe0*ne0 + μu0*nu0 + μd0*nd0 - P0
 
     # convert units
@@ -141,31 +145,72 @@ def eos(neutral, μside, name="ϵ"):
     ϵ0 /= constants.ϵ0 # to TOV-dimensionless units
     P0 /= constants.ϵ0 # to TOV-dimensionless units
 
+    # cut away 0, linearly interpolate
+    i = np.argmax(ϵ0 > 3e-4)
+    ϵ0 = ϵ0[i:]
+    P0 = P0[i:]
+    ϵx = ϵ0[1]-(ϵ0[2]-ϵ0[1])/(P0[2]-P0[1])*P0[1] # linear interpolate ϵ at P=0
+    P0 = np.concatenate([[0], P0])
+    ϵ0 = np.concatenate([[ϵx], ϵ0])
+
     ϵ = scipy.interpolate.interp1d(P0, ϵ0)
     ϵ.__name__ = name
-    return ϵ
 
-P = np.linspace(0, 10, 1000)
-ϵ = eos(False, "", name="ϵ")
-plt.plot(P, ϵ(P), "-r.")
-plt.show()
+    def ϵf(P): return 0 if P <= 0 else ϵ(P)
 
-opts = { "tolD": 0.05, "tolP": 1e-5, "maxdr": 1e-3, "Psurf": 0, "visual": True }
-massradiusplot(ϵ1, (1e-1, 1e4), **opts, nmodes=0)
+    if plotfirst:
+        plt.plot(P0, ϵ(P0), "-k")
+        plt.plot(P0, ϵ0, " r.")
+        #plt.xlim(0, 100)
+        #plt.ylim(0, 100)
+        #plt.xticks(np.linspace(0, 1, 11))
+        #plt.yticks(np.linspace(0, 1, 11))
+        plt.show()
+
+    return ϵf
+
+plotfirst = True
+#ϵ1 = eos(False, "", False, name="ϵ", plotfirst=plotfirst)
+ϵ2 = eos(True, "right", False, B=0, name="ϵ", plotfirst=plotfirst)
+#ϵ3 = eos(True, "right", True, name="ϵ", plotfirst=plotfirst)
+#P = np.linspace(0, 1, 5000)
+#plt.plot(P, ϵ1(P), "-r.")
+#plt.plot(P, ϵ2(P), "-g.")
+#plt.plot(P, ϵ3(P), "-b.")
+#plt.show()
+
+#opts = { "tolD": 0.10, "tolP": 1e-5, "maxdr": 1e-2, "Psurf": 0, "visual": True }
+#massradiusplot(ϵ1, (1e-5, 1e-4), **opts, nmodes=0)
+
+# TODO: test bag model
+#B=0
+#def ϵ(P): return 3*P + 4*B/constants.ϵ0
+#opts = { "tolD": 0.10, "tolP": 1e-5, "maxdr": 1e-3, "Psurf": 0, "visual": True }
+#massradiusplot(ϵ, (1e-6, 1e-2), **opts, nmodes=0)
+
+
+opts = { "tolD": 0.10, "tolP": 1e-5, "maxdr": 1e-3, "Psurf": 0, "visual": True }
+massradiusplot(ϵ2, (1e-6, 1e-2), **opts, nmodes=0)
+
+#opts = { "tolD": 0.10, "tolP": 1e-5, "maxdr": 1e-3, "Psurf": 0, "visual": True }
+#massradiusplot(ϵ3, (1e2, 1e4), **opts, nmodes=0)
 
 # grand potential
+neutral = True
+μside = "right" # left or right
+neglecte = True
 σ = np.linspace(0, +200, 100)[1:]
 μu = np.linspace(0, 600, 200)
 μuμu, σσ = np.meshgrid(μu, σ)
 if True:
-    σ, μu, μd, μe, ω = σμω(σσ, μuμu, neutral, μside)
+    σ, μu, μd, μe, ω = σμω(σσ, μuμu, neutral, μside, neglecte)
     ωc = ω.reshape(-1)
     μuc = μu.reshape(-1)
     σc = σ.reshape(-1)
     utils.writecols([σc, μuc, ωc / fπ**4], ["sigma", "muu", "omega"], "data/lsmpot.dat", skipevery=np.shape(ω)[1])
     σ, μu, μd, μe = σ[:,0], μu[0,:], μd[0,:], μe[0,:]
     plt.plot(σ, ω / fπ**4, "-k")
-ret = σμω0(μu, neutral, μside)
+ret = σμω0(μu, neutral, μside, neglecte)
 σ0, μu0, μd0, μe0, ω0 = ret[:,0], ret[:,1], ret[:,2], ret[:,3], ret[:,4]
 pti = np.argmax(np.abs(σ0[1:]-σ0[:-1])) + 1 # pti is now first point after phase transition
 
