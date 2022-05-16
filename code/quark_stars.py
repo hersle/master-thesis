df76256e2a13c701a5944f21b4ca3c0bba23924c
Herman Sletmoen <hermansletmoen@gmail.com>
2022-05-16 17:47:17 +0200

    Done for the day

--- code/quark_stars.py
+++ code/quark_stars.py
@@ -33,7 +33,7 @@ nsat = 0.165
 σx0 = fπ
 σy0 = np.sqrt(2)*fK-fπ/np.sqrt(2)
 
-tovopts = {"tolD": 0.001, "maxdr": 1e-2, "nmodes": 0}
+tovopts = {"tolD": 0.02, "maxdr": 1e-2, "nmodes": 0}
 tovμQ = np.concatenate([np.linspace(0, 700, 200)[1:], np.linspace(700, 5000, 100)])
 
 def charge(mu, md, ms, μu, μd, μs, μe):
@@ -920,8 +920,8 @@ if __name__ == "__main__":
 
     # hybrid model (3-flavor quark-meson model + APR hadronic EOS)
     #HybridModel(mσ=600).eos(B=111**4, plot=True, write=True)
-    HybridModel(mσ=600).stars(111, (1e-5, 1e-2), write=True) # use tolD=0.01
-    HybridModel(mσ=700).stars(68,  (1e-5, 1e-2), write=True) # use tolD=0.01
-    HybridModel(mσ=800).stars(27,  (1e-5, 1e-2), write=True) # use tolD=0.01
+    #HybridModel(mσ=600).stars(111, (1e-5, 1e-2), write=True) # use tolD=0.001
+    #HybridModel(mσ=700).stars(68,  (1e-5, 1e-2), write=True) # use tolD=0.001
+    #HybridModel(mσ=800).stars(27,  (1e-5, 1e-2), write=True) # use tolD=0.001
     #HybridModel(mσ=800).star(0.0012587499999999999, 27, plot=True, write=True)
     #exit()
